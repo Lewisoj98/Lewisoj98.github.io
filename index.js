3a48e20e665710ee64a04843581dfaf391e2a758
Lewis Oxley-Jenkins <lewis.oxley98@outlook.com>
2021-06-12 11:25:33 +0100

    smooth scroll

---
+++ index.js
@@ -0,0 +1,23 @@
+$(document).ready(()=>{
+    'use strict'
+    $('a[href="#about"]').click(()=>{
+        $('html,body').animate({
+            scrollTop: $('#about').offset().top
+        }, 'slow')
+    })
+    $('a[href="#skills"]').click(()=>{
+        $('html,body').animate({
+            scrollTop: $('#skills').offset().top
+        }, 'slow')
+    })
+    $('a[href="#projects"]').click(()=>{
+        $('html,body').animate({
+            scrollTop: $('#projects').offset().top
+        }, 'slow')
+    })
+    $('a[href="#contact"]').click(()=>{
+        $('html,body').animate({
+            scrollTop: $('#contact').offset().top
+        }, 'slow')
+    })
+})
