68a3e7bb25f2bae6bbbcecb2bb9e39e10946dee2
Lewis Oxley-Jenkins <lewis.oxley98@outlook.com>
2021-09-17 13:35:28 +0100

    Added SM links and CV

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-$(document).ready(()=>{
+$(document).ready(() => {
     'use strict'
-    $('a[href="#about"]').click(()=>{
+    $('a[href="#about"]').click(() => {
         $('html,body').animate({
             scrollTop: $('#about').offset().top
         }, 'slow')
     })
-    $('a[href="#skills"]').click(()=>{
+    $('a[href="#skills"]').click(() => {
         $('html,body').animate({
             scrollTop: $('#skills').offset().top
         }, 'slow')
     })
-    $('a[href="#projects"]').click(()=>{
+    $('a[href="#projects"]').click(() => {
         $('html,body').animate({
             scrollTop: $('#projects').offset().top
         }, 'slow')
     })
-    $('a[href="#contact"]').click(()=>{
+    $('a[href="#contact"]').click(() => {
         $('html,body').animate({
             scrollTop: $('#contact').offset().top
         }, 'slow')
     })
-    $('a[href="#"]').click(()=>{
+    $('a[href="#"]').click(() => {
         $('html,body').animate({
             scrollTop: $('body').offset().top
         }, 'slow')
@@ -28,14 +28,15 @@ $(document).ready(()=>{
 })
 
 $(window).scroll(function () {
-    if($(window).scrollTop() > 0) {
+    if ($(window).scrollTop() > 0) {
         $('#about').addClass("animate");
-    }
-    else {
-        $('#about').removeClass("animate");
     }
 })
 
-$('.card').on('touchstart', function() {
-    this.toggleClass(":hover");
+$('.card').on('touchstart', function () {
+    if (this.hasClass(":hover")) {
+        this.removeClass(":hover");
+    } else {
+        this.toggleClass(":hover");
+    }
 })
